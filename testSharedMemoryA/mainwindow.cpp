eca4454eb58e5373693ae40729ba791059d4a566
shenmingyi <1006714596@qq.com>
2019-07-25 19:50:33 +0800

    qt_sharedMemory

---
+++ testSharedMemoryA/mainwindow.cpp
@@ -0,0 +1,47 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    this->setWindowTitle("共享内存创建者");
+
+    m_ = new QSharedMemory();
+
+    m_->setKey("smy");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+    if(m_->isAttached())
+    {
+        if(!m_->detach())
+            qDebug()<<"detach error";
+        return;
+    }
+
+    if(m_->create(1024))//共享内存
+    {
+
+        m_->lock();
+
+        char* d = (char*)m_->data();
+
+        d = "hello";
+
+        memcpy(m_->data(),d,strlen(d)*sizeof(char));
+
+        m_->unlock();
+    }
+}
+
+
---
+++ testSharedMemoyB/mainwindow.cpp
@@ -0,0 +1,40 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    m_ = new QSharedMemory();
+    this->setWindowTitle("共享内存读取");
+
+    m_->setKey("smy");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+    m_->lock();
+
+    if(!m_->attach())
+    {
+        qDebug()<<"attach error!";
+        return;
+    }
+
+    char*d;
+    d = (char *)m_->constData();
+
+    qDebug()<<d;
+
+    m_->unlock();
+
+    m_->detach();
+}
+
